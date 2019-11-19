494dc40eec3cf90e5950df7f380f5eb2e9ac04c1
Abhishek Singh Thakur <mailabhi593@gmail.com>
2019-11-19 22:22:37 +0530

    Update selectionsort.cpp

--- Selection Sort/C++/selectionsort.cpp
+++ Selection Sort/C++/selectionsort.cpp
@@ -1,54 +1,60 @@
 #include <iostream>
- #include <vector>
- #include <algorithm>
- #include <iterator>
+#include <vector>
 
- template <typename ForwardItr>
- void selectionSort(ForwardItr first, ForwardItr last)
- {
-   while(first != last)
-   {
-     ForwardItr min = std::min_element(first, last);
-       std::iter_swap(min, first);
-     ++first;
-   }
- }
-
- template <typename Itr>
- void print(Itr first, Itr last)
- {
-   while(first != last)
-   {
-     std::cout << *first <<" ";
-     ++first;
-   }
-   std::cout << "\n";
- }
-
- int main()
- {
-   std::vector<int> v({5, 3, 12, 2, 8});
-   std::cout << "Original Array :";
-   print(v.begin(), v.end());
-   selectionSort(v.begin(), v.end());
-   std::cout <<"Sorted Array :";
-   print(v.begin(), v.end());
-   std::cout << '\n';
-
-   std::vector<char> c({'t', 'q', 'a', 'r', 'p'});
-   std::cout << "Original Array :";
-   print(c.begin(), c.end());
-   selectionSort(c.begin(), c.end());
-   std::cout <<"Sorted Array :";
-   print(c.begin(), c.end());
-   std::cout << '\n';
-
-   std::vector<std::string> str({"code", "live", "love", "sing", "create"});
-   std::cout << "Original Array :";
-   print(str.begin(), str.end());
-   selectionSort(str.begin(), str.end());
-   std::cout <<"Sorted Array :";
-   print(str.begin(), str.end());
-   std::cout << '\n';
-
+template<typename T>
+void selection_sort(std::vector<T>& array)
+{
+    typedef typename std::vector<T>::iterator Itr;
+    Itr itr = array.begin();
+    while(itr != array.end())
+    {
+        Itr itr_min = itr;
+        for(Itr i = itr + 1; i != array.end(); i++)
+        {
+            if(*i < *itr_min)
+            {
+                itr_min = i;
+            }
+        }
+        std::iter_swap(itr, itr_min);
+        itr++;
+    }
 }
+
+template<typename T>
+void print(const std::vector<T>& array)
+{
+    for(auto itr = array.begin(); itr != array.end(); itr++)
+    {
+       std::cout << *itr << " ";
+    }
+    std::cout << '\n';
+}
+
+int main()
+{
+    std::vector<int> v({5, 3, 12, 2, 8});
+    std::cout << "Original Array :";
+    print(v);
+    selection_sort(v);
+    std::cout <<"Sorted Array :";
+    print(v);
+    std::cout << '\n';
+
+    std::vector<char> c({'t', 'q', 'a', 'r', 'p'});
+    std::cout << "Original Array :";
+    print(c);
+    selection_sort(c);
+    std::cout <<"Sorted Array :";
+    print(c);
+    std::cout << '\n';
+
+    std::vector<std::string> str({"code", "live", "love", "sing", "create"});
+    std::cout << "Original Array :";
+    print(str);
+    selection_sort(str);
+    std::cout <<"Sorted Array :";
+    print(str);
+    std::cout << '\n';
+}
+
