598463a912c1e0c966a962292a6ea1c38bf7e970
Abhishek Singh Thakur <mailabhi593@gmail.com>
2019-11-21 14:42:38 +0530

    Update doublylinkedlist.cpp

--- Doubly Linked List/C++/doublylinkedlist.cpp
+++ Doubly Linked List/C++/doublylinkedlist.cpp
@@ -1,77 +1,87 @@
-//www.programmercave.blogspot.com
 #include <iostream>
 
 template<class T>
-class DoublyLinkedList{
-  struct Node{
-  T data;
-  Node* next;
-  Node* prev;
-  Node(T val):data(val),next(nullptr),prev(nullptr){}
-  };
-  Node *head ,*tail;
+class DoublyLinkedList
+{
+    struct Node
+    {
+        T data;
+        Node* next;
+        Node* prev;
+        Node(T val): data(val), next(nullptr), prev(nullptr) {}
+    };
+    Node *head, *tail;
 
   public:
-      DoublyLinkedList():head(nullptr),tail(nullptr){}
+      DoublyLinkedList(): head(nullptr), tail(nullptr) {}
 
-     ~DoublyLinkedList(){
-        Node *tmp = nullptr;
-          while (head){
-             tmp = head;
-             head = head->next;
-             delete tmp;
+     ~DoublyLinkedList()
+      {
+          Node *tmp = nullptr;
+          while (head)
+          {
+              tmp = head;
+              head = head->next;
+              delete tmp;
           }
-          head =nullptr;
-        }
+          head = nullptr;
+      }
 
       DoublyLinkedList(const DoublyLinkedList<T> & dll) = delete;
       DoublyLinkedList& operator=(DoublyLinkedList const&) = delete;
-      void insertFront(T val){
-         Node *node = new Node(val);
+
+      void insertFront(T val)
+      {
+          Node *node = new Node(val);
           Node *tmp = head;
-          if (head == nullptr){
+          if (head == nullptr)
+          {
               head = node;
-              tail=node;
-              }
-         else{
-            node->next=head;
-            head=node;
-            node->next->prev = node;
-             while (tmp->next != nullptr){
-               tmp = tmp->next;
-             }
-          tail=tmp;
-        }
-    }
-
-      void insertBack(T val){
-         Node *node=new Node(val);
-           if(tail->next==nullptr){
-            tail->next=node;
-            tail=node;
-            }
-     }
-
-
-     void deleteVal(T val){
-      Node* find = findVal(val);
-      Node *tmp = head;
-        if(tmp==find){
-          head=tmp->next;
-        }
-        else{
-           while(find != nullptr){
-              if(tmp->next==find){
-              tmp->next = find->next;
-              find->next->prev = tmp;
-              delete find;
-              return;
-              }
-            tmp=tmp->next;
+              tail = node;
           }
-       }
+          else
+          {
+              node->next = head;
+              head = node;
+              node->next->prev = node;
+          }
+      }
 
-    }
+      void insertBack(T val)
+      {
+          Node *node = new Node(val);
+          if(tail->next == nullptr)
+          {
+              tail->next = node;
+              tail = node;
+          }
+      }
+
+
+     void deleteVal(T val)
+     {
+          Node* find = findVal(val);
+          Node *tmp = head;
+
+          if(tmp == find)
+          {
+              head = tmp->next;
+          }
+          else
+          {
+              while(find != nullptr)
+              {
+                  if(tmp->next == find)
+                  {
+                      tmp->next = find->next;
+                      find->next->prev = tmp;
+                      delete find;
+                      return;
+                  }
+                  tmp = tmp->next;
+              }
+          }
+      }
 
      template <class U>
      friend std::ostream & operator<<(std::ostream & os, const DoublyLinkedList<U> & dll){
@@ -81,25 +91,31 @@ class DoublyLinkedList{
 
      private:
 
-         Node *findVal(T n){    //returns node of the given number
-         Node *node = head;
-         while(node!=nullptr){
-            if(node->data==n)
-                 return node;
-         node=node->next;
-         }
-         std::cerr<<"No such element in the list \n";
-         return nullptr;
-       }
+         Node *findVal(T n) //returns node of the given number
+         {    
+              Node *node = head;
+              while(node != nullptr)
+              {
+                    if(node->data == n)
+                          return node;
 
-       void display(std::ostream& out = std::cout) const{
-        Node *node = head;
-        while(node!=nullptr){
-        out <<node->data << " ";
-        node=node->next;
-        }
-      }
+                    node = node->next;
+              }
+              std::cerr << "No such element in the list \n";
+              return nullptr;
+          }
+
+       void display(std::ostream& out = std::cout) const
+       {
+            Node *node = head;
+            while(node != nullptr)
+            {
+                out << node->data << " ";
+                node = node->next;
+            }
+        } 
 };
+
 int main(){
   DoublyLinkedList<int> l1;
   l1.insertFront(3);
@@ -108,7 +124,7 @@ int main(){
   l1.insertFront(6);
   l1.insertBack(88);
   std::cout<<l1<<"\n";
-  l1.deleteVal(12);
+  l1.deleteVal(11);
    std::cout<<l1<<"\n";
   return 0;
 }
