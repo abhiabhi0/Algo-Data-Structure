bbb56df25d0b8312f7cb7fee5019c56fdf89dc48
Abhishek Singh Thakur <mailabhi593@gmail.com>
2022-03-04 14:28:07 +0530

    Create gcd.cpp

---
+++ Maths/gcd.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+int gcd(int a, int b)
+{
+	if (a == 0) return b;
+
+	return gcd(b % a, a);
+}
+
+int main()
+{
+	#ifndef FILE_INOUT
+ 
+    	freopen("in.txt", "r", stdin);
+ 	
+    	freopen("out.txt", "w", stdout);
+   
+	#endif
+
+    int a, b;
+    std::cin >> a >> b;
+
+    std::cout << gcd(a, b) << "\n";
+
+    return 0;
+}
