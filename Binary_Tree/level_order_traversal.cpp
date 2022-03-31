aef85fa2cc6c3e741bd28572fa58adbc9dd8f448
Abhishek Singh Thakur <mailabhi593@gmail.com>
2022-03-31 11:49:54 +0530

    Create level_order_traversal.cpp

---
+++ Binary_Tree/level_order_traversal.cpp
@@ -0,0 +1,39 @@
+std::vector<vector<int>> level_order_traversal(TreeNode* root)
+{
+	if (root == NULL)
+		return NULL;
+
+	std::vector<vector<int>> res;
+
+	std::queue<TreeNode*> q;
+	q.push(root);
+
+	while (!q.empty())
+	{
+		std::vector<int> level;
+		int q_sz = q.size();
+
+		for (int i = 0; i < q_sz; ++i)
+		{
+			TreeNode* tmp = q.front();
+			q.pop();
+
+			if (i == 0)
+			{
+				level.push(tmp->val);
+			}
+
+			if (tmp->left != NULL)
+			{
+				q.push(tmp->left);
+			}
+
+			if (tmp->right != NULL)
+			{
+				q.push(tmp->right);
+			}
+		}
+		res.push_back(level);
+	}
+	return res;
+}
