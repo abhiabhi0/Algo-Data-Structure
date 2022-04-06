cb129aa02468ab7564c4c762bbb980d34210ae3a
Abhishek Singh Thakur <mailabhi593@gmail.com>
2022-04-06 10:49:40 +0530

    Create nCr_mod_m.cpp

---
+++ Combinatorics/nCr_mod_m.cpp
@@ -0,0 +1,24 @@
+//using Dynamic Programming
+//A = n, B = r and C = m (mod)
+int nCr_mod_m_DP(int A, int B, int C) {
+    if (B == 1) return A % C;
+
+    if (B == A || B == 0) return 1;
+
+    vector< vector<int> > mat(A+1, vector<int>(B+1, 0));
+
+    for (int i = 0; i <= A; ++i)
+    {
+        mat[i][0] = 1;
+    }
+
+    for (int i = 1; i <= A; ++i)
+    {
+        for (int j = 1; j <= B; ++j)
+        {
+            mat[i][j] = (mat[i-1][j] + mat[i-1][j-1]) % C;
+        }
+    }
+
+    return mat[A][B] % C;
+}
