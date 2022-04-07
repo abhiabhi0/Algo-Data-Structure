df6b9e4ccfbe4747f0cf8aa4c2a458c135c15147
Abhishek Singh Thakur <mailabhi593@gmail.com>
2022-04-07 13:40:25 +0530

    Update tree_diameter.cpp

--- Binary_Tree/tree_diameter.cpp
+++ Binary_Tree/tree_diameter.cpp
@@ -3,27 +3,52 @@
  * of the tree.
 **/
 
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
 class NodeInfo
 {
-	int height;
-	int diameter;
+    public:
+    int height;
+    int diameter;
+  
+    NodeInfo(int h, int d) : height(h),
+                             diameter(d)
+                             {}
+};
 
-  public:
-  	NodeInfo(int ht, int dia) : height(ht),
-  								diameter(dia)
-  								{}
-}
-
-NodeInfo tree_diameter(TreeNode* root)
+int tree_height(TreeNode* A)
 {
-	if (root == NULL)
-	{
-		return new NodeInfo(-1, -1);
-	}
+    if (A == NULL)
+    {
+        return -1;
+    }
 
-	NodeInfo l = tree_diameter(root->left);
-	NodeInfo r = tree_diameter(root->right);
-
-	return new NodeInfo(max(l.height, r.height)+1,
-		max(max(l.diameter, r.diameter), (l.height + r.height+2)));
+    return 1 + max(tree_height(A->left), tree_height(A->right));
 }
+
+NodeInfo tree_diameter(TreeNode* A)
+{
+    if (A == NULL)
+    {
+        return NodeInfo(-1, -1);
+    }
+
+    NodeInfo l = tree_diameter(A->left);
+    NodeInfo r = tree_diameter(A->right);
+
+    return NodeInfo(max(l.height, r.height)+1, max(max(l.diameter, r.diameter), l.height + r.height + 2));
+}
+
+int solve(TreeNode* A) {
+    NodeInfo obj = tree_diameter(A);
+    return obj.diameter;
+}
+
